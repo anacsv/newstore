39725f9c5bd71117907726d4cccabfcb3cb7bdd1
anacsv <anapaula.carneiro@olist.com>
2020-07-31 22:27:01 -0300

    Adjust classes of Person

---
+++ model/item.py
@@ -0,0 +1,36 @@
+from .base_model import Base
+
+class Item(Base):
+    def __init__(self, name:str = '', price:float = 0.0, description:str = '', id:int = 0):
+        self.__name = name
+        self.__price = price
+        self.__description = description
+        super().__init__(id)
+
+    # Getters and Setters Name
+    @property
+    def name(self)->str:
+        return self.__name
+
+    @name.setter
+    def name(self, name:str)->None:
+        self.__name = name
+
+    # Getters and Setters Price
+    @property
+    def price(self)->float:
+        return self.__price
+
+    @price.setter
+    def price(self, price:float)->None:
+        self.__price = price
+
+    # Getters and Setters Description
+    @property
+    def description(self)->str:
+        return self.__description
+
+    @description.setter
+    def description(self, description:str)->None:
+        self.__description = description
+
--- model/person.py
+++ model/person.py
@@ -1,26 +1,36 @@
 from .base_model import Base
+from .type_person import TypePerson
+
 
 class Person(Base):
-    def __init__(self, name:str = '', date_of_birth:str = '', id:int = 0):
+    def __init__(self, name: str = '', date_of_birth: str = '', type_person:TypePerson = None, id: int = 0):
         self.__name = name
         self.__date_of_birth = date_of_birth
         super().__init__(name, date_of_birth, id)
 
     # Getters and Setters Name
     @property
-    def name(self)->str:
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name:str):
+    def name(self, name: str):
         self.__name = name
 
     # Getters and Setters Date of Birth
     @property
-    def date_of_birth(self)->str:
+    def date_of_birth(self) -> str:
         return self.__date_of_birth
 
     @date_of_birth.setter
-    def date_of_birth(self, date_of_birth:str):
+    def date_of_birth(self, date_of_birth: str):
         self.__date_of_birth = date_of_birth
 
+    # Getters and Setters Type Person
+    @property
+    def type_person(self) -> TypePerson:
+        return self.__type_person
+
+    @type_person.setter
+    def type_person(self, type_person: TypePerson):
+        self.__type_person = type_person
